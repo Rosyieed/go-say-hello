3782bd9941ccd96c05ba3086ea2b0ebb132afdb7
Rosyieed <rosyid123.ren@gmail.com>
2024-12-24 11:31:30 +0700

    update say hello with parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Rosyieed/go-say-hello
+module github.com/Rosyieed/go-say-hello/v2
 
 go 1.23.1
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Say Hello Go Function"
+func SayHello(name string) string {
+	return "Say Hello " + name
 }
